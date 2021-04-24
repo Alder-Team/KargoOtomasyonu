aa7d302bc30e745f9a8d6438712dbf128f1c6359
Gulnur Ogur <gulnurogur@gmail.com>
2021-04-24 15:01:47 +0300

    krgalicibilgileriyoneticisi düzenlendi.

--- Veri/VeriYonetisi/krgalicibilgileriyoneticisi.cpp
+++ Veri/VeriYonetisi/krgalicibilgileriyoneticisi.cpp
@@ -1,6 +1,21 @@
 #include "krgalicibilgileriyoneticisi.h"
+#include <Veri/VeriListesi/krgalicibilgileri.h>
 
 KRGAliciBilgileriYoneticisi::KRGAliciBilgileriYoneticisi(QObject *parent) : QObject(parent)
 {
 
+}
+
+KRGAliciBilgileriYoneticisi::Ptr KRGAliciBilgileriYoneticisi::kopyaOlustur(TemelVeriYonetici::Ptr kaynak)
+{
+    Ptr kopya = yeni();
+
+    kopya->setId(kaynak->getId());
+    kopya->setAliciAdi(kaynak->getAliciAdi());
+    kopya->setAliciSoyadi(kaynak->getAliciSoyadi());
+    kopya->setAliciAdresi(kaynak->getAliciAdresi());
+    kopya->setAliciTelNo(kaynak->getAliciTelNo());
+    kopya->setAliciEmail(kaynak->getAliciEmail());
+
+    return kopya;
 }
--- Veri/VeriYonetisi/krgalicibilgileriyoneticisi.h
+++ Veri/VeriYonetisi/krgalicibilgileriyoneticisi.h
@@ -2,12 +2,17 @@
 #define KRGALICIBILGILERIYONETICISI_H
 
 #include <QObject>
+#include <Veri/VeriYonetisi/temel_veri_yoneticisi.h>
 
-class KRGAliciBilgileriYoneticisi : public QObject
+class KRGAliciBilgileriYoneticisi : public QObject,
+                                    public TemelVeriYonetici <KRGAliciBilgileri, KRGAliciBilgileriPtr>
 {
     Q_OBJECT
 public:
     explicit KRGAliciBilgileriYoneticisi(QObject *parent = nullptr);
+
+    // Kopya oluşturur.
+    Ptr kopyaOlustur(Ptr kaynak);
 
 signals:
 
