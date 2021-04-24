4c65c42431ba9e6b5039cd92e4f38181c62f3e46
Ali Eren Ekinci <mralierenekinci@gmail.com>
2021-04-24 21:08:52 +0300

    krgsubebilgileriyoneticisi.h ve krgsubebilgileriyoneticisi.cpp düzenlendi.

--- Veri/VeriYonetisi/krgsubebilgileriyoneticisi.cpp
+++ Veri/VeriYonetisi/krgsubebilgileriyoneticisi.cpp
@@ -1,6 +1,20 @@
 #include "krgsubebilgileriyoneticisi.h"
+#include <Veri/VeriListesi/krgsubebilgileri.h>
 
 KRGSubeBilgileriYoneticisi::KRGSubeBilgileriYoneticisi(QObject *parent) : QObject(parent)
 {
 
+}
+
+KRGSubeBilgileriYoneticisi::Ptr KRGSubeBilgileriYoneticisi::kopyaOlustur(TemelVeriYonetici::Ptr kaynak)
+{
+    Ptr kopya = yeni();
+    kopya->setId(kaynak->getId());
+    kopya->setSubeAdi(kaynak->getSubeAdi());
+    kopya->setSubeAdresi(kaynak->getSubeAdresi());
+    kopya->setSubeTelNo(kaynak->getSubeTelNo());
+    kopya->setSubeYetkilisi(kaynak->getSubeYetkilisi());
+
+
+    return kopya;
 }
--- Veri/VeriYonetisi/krgsubebilgileriyoneticisi.h
+++ Veri/VeriYonetisi/krgsubebilgileriyoneticisi.h
@@ -2,12 +2,14 @@
 #define KRGSUBEBILGILERIYONETICISI_H
 
 #include <QObject>
+#include <Veri/VeriYonetisi/temel_veri_yoneticisi.h>
 
-class KRGSubeBilgileriYoneticisi : public QObject
+class KRGSubeBilgileriYoneticisi : public QObject, public TemelVeriYonetici <KRGSubeBilgileri, KRGSubeBilgileriPtr>
 {
     Q_OBJECT
 public:
     explicit KRGSubeBilgileriYoneticisi(QObject *parent = nullptr);
+    Ptr kopyaOlustur(Ptr kaynak);
 
 signals:
 
