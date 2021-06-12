4376f682d0e78c774c5ba7a39f0147734f12b117
Huseyin Mutlu <huseyin.mtl0@hotmail.com>
2021-06-12 12:56:07 +0300

    update -veriformları

--- UI/VeriFormlari/krgyenikargoekle.cpp
+++ UI/VeriFormlari/krgyenikargoekle.cpp
@@ -1,6 +1,10 @@
 #include "krgyenikargoekle.h"
 #include "ui_krgyenikargoekle.h"
 
+#include <Veri/VeriListesi/krgkargobilgileri.h>
+#include <Veri/VeriListesi/krgalicibilgileri.h>
+#include <Veri/VeriListesi/krggondericibilgileri.h>
+
 KRGYeniKargoEkle::KRGYeniKargoEkle(QWidget *parent) :
     QDialog(parent),
     ui(new Ui::KRGYeniKargoEkle)
@@ -12,3 +16,105 @@ KRGYeniKargoEkle::~KRGYeniKargoEkle()
 {
     delete ui;
 }
+
+KRGKargoBilgileriPtr KRGYeniKargoEkle::getVeriKargo() const
+{
+    if (ui->radiobtnAliciOdemeli->isChecked()) {
+        veriKargo->setOdemeTuru(OTAliciOdemeli);
+    } else {
+        veriKargo->setOdemeTuru(OTGondericiOdemeli);
+    }
+
+    veriKargo->setKargoEn(ui->dspinboxDesiEn->value());
+    veriKargo->setKargoBoy(ui->dspinboxDesiBoy->value());
+    veriKargo->setKargoYukseklik(ui->dspinboxDesiYukseklik->value());
+    veriKargo->setKargoAgirlik(ui->dspinboxDesiAgirlik->value());    //Değişebilir
+
+    veriKargo->setKargoDesi(ui->lblDesiSonucSayi->text().toDouble());
+
+    veriKargo->setGonderenSube(ui->comboboxGondericiSube->currentText());
+    // ID alabiliriz
+    veriKargo->setAliciSube(ui->comboboxAliciSube->currentText());
+
+    veriKargo->setKargoTarihi(ui->dateEditTarih->date());
+
+    veriKargo->setKargoUcreti(ui->lblTutarSonuc->text().toDouble());
+
+    return veriKargo;
+}
+
+void KRGYeniKargoEkle::setVeriKargo(const KRGKargoBilgileriPtr &value)
+{
+    veriKargo = value;
+
+    switch (veriKargo->getOdemeTuru()) {
+    case OTGondericiOdemeli:
+        ui->radiobtnAliciOdemeli->setChecked(true);
+        break;
+    default:
+        ui->radiobtnAliciOdemeli->setChecked(true);
+    }
+
+    ui->dspinboxDesiEn->setValue(veriKargo->getKargoEn());
+    ui->dspinboxDesiBoy->setValue(veriKargo->getKargoBoy());
+    ui->dspinboxDesiYukseklik->setValue(veriKargo->getKargoYukseklik());
+    ui->dspinboxDesiAgirlik->setValue(veriKargo->getKargoAgirlik());
+    ui->lblDesiSonucSayi->setText(tr("%1").arg(veriKargo->getKargoDesi()));
+
+    ui->comboboxGondericiSube->setCurrentText(veriKargo->getGonderenSube());
+    ui->comboboxAliciSube->setCurrentText(veriKargo->getAliciSube());
+
+    ui->dateEditTarih->setDate(veriKargo->getKargoTarihi());
+    ui->lblTutarSonuc->setText(tr("%1").arg(veriKargo->getKargoUcreti()));
+}
+
+KRGGondericiBilgileriPtr KRGYeniKargoEkle::getVeriGonderici() const
+{
+    return veriGonderici;
+}
+
+void KRGYeniKargoEkle::setVeriGonderici(const KRGGondericiBilgileriPtr &value)
+{
+    veriGonderici = value;
+}
+
+KRGAliciBilgileriPtr KRGYeniKargoEkle::getVeriAlici() const
+{
+    return veriAlici;
+}
+
+void KRGYeniKargoEkle::setVeriAlici(const KRGAliciBilgileriPtr &value)
+{
+    veriAlici = value;
+}
+
+//KRGKargoBilgileriPtr KRGYeniKargoEkle::getVeri() const
+//{
+//    if (ui->radiobtnAliciOdemeli->isChecked()) {
+//        veri->setOdemeTuru(OTAliciOdemeli);
+//    } else {
+//        veri->setOdemeTuru(OTGondericiOdemeli);
+//    }
+
+//    veri->setKargoEn(ui->dspinboxDesiEn->value());
+//    veri->setKargoBoy(ui->dspinboxDesiBoy->value());
+//    veri->setKargoYukseklik(ui->dspinboxDesiYukseklik->value());
+//    veri->setKargoAgirlik(ui->dspinboxDesiAgirlik->value());    //Değişebilir
+
+//    veri->setKargoDesi(ui->lblDesiSonucSayi->text().toDouble());
+
+//    veri->setGonderenSube(ui->comboboxGondericiSube->currentText());
+//    // ID alabiliriz
+//    veri->setAliciSube(ui->comboboxAliciSube->currentText());
+
+//    veri->setKargoTarihi(ui->dateEditTarih->date());
+
+//    veri->setKargoUcreti(ui->lblTutarSonuc->text().toDouble());
+
+//    return veri;
+//}
+
+//void KRGYeniKargoEkle::setVeri(const KRGKargoBilgileriPtr &value)
+//{
+//    veri = value;
+//}
--- UI/VeriFormlari/krgyenikargoekle.h
+++ UI/VeriFormlari/krgyenikargoekle.h
@@ -2,6 +2,7 @@
 #define KRGYENIKARGOEKLE_H
 
 #include <QDialog>
+#include <Veri/tanimlar.h>
 
 namespace Ui {
 class KRGYeniKargoEkle;
@@ -15,8 +16,24 @@ public:
     explicit KRGYeniKargoEkle(QWidget *parent = nullptr);
     ~KRGYeniKargoEkle();
 
+//    KRGKargoBilgileriPtr getVeri() const;
+//    void setVeri(const KRGKargoBilgileriPtr &value);
+
+    KRGKargoBilgileriPtr getVeriKargo() const;
+    void setVeriKargo(const KRGKargoBilgileriPtr &value);
+
+    KRGGondericiBilgileriPtr getVeriGonderici() const;
+    void setVeriGonderici(const KRGGondericiBilgileriPtr &value);
+
+    KRGAliciBilgileriPtr getVeriAlici() const;
+    void setVeriAlici(const KRGAliciBilgileriPtr &value);
+
 private:
     Ui::KRGYeniKargoEkle *ui;
+
+    KRGKargoBilgileriPtr veriKargo;
+    KRGGondericiBilgileriPtr veriGonderici;
+    KRGAliciBilgileriPtr veriAlici;
 };
 
 #endif // KRGYENIKARGOEKLE_H
